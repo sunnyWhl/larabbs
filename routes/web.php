fb3887f7e80ea0a9efaeae953dc63914a2aa4d19
vagrant <vagrant@homestead>
2019-04-25 07:46:26 +0000

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
 
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
 
